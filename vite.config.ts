6d63c9ab540f7ca1197ac89c4e68b2effb7bf4e6
Danial <sdnoaein@gmail.com>
2023-09-15 00:03:26 +0330

    fix: base url

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "https://danialnoaein.github.io/filter-app-front/",
+  base: "https://danialnoaein.github.io/filter-app-front/",
 });
