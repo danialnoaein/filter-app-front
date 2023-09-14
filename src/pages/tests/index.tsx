966b86dd906bee48a773b7d0cd275a624d7f84fa
Danial <sdnoaein@gmail.com>
2023-09-14 23:40:53 +0330

    feat: add more sections in tests

--- src/pages/tests/index.tsx
+++ src/pages/tests/index.tsx
@@ -20,6 +20,12 @@ const Tests = () => {
         </Typography>
         <Typography>اول خودت رو بشناس، بعد تصمیم بگیر </Typography>
       </div>
+
+      <div
+        style={{ fontWeight: "bold", fontSize: "1.2rem", marginTop: "16px" }}
+      >
+        تله های زندگی
+      </div>
       <div>
         <div style={{ overflow: "auto", whiteSpace: "nowrap" }}>
           <Card
@@ -59,7 +65,6 @@ const Tests = () => {
               <Button size='small'>شروع</Button>
             </CardActions>
           </Card>
-
           <Card
             style={{
               display: "inline-block",
@@ -97,7 +102,127 @@ const Tests = () => {
               <Button size='small'>شروع</Button>
             </CardActions>
           </Card>
+          <Card
+            style={{
+              display: "inline-block",
+              marginLeft: "8px",
+              width: "180px",
+            }}
+          >
+            <CardMedia
+              sx={{ height: 140 }}
+              image='https://nextluxury.com/wp-content/uploads/couple-not-talking-to-each-other-after-quarrel.jpg'
+            />
+            <CardContent sx={{ paddingBottom: "0px" }}>
+              <Typography
+                variant='body2'
+                sx={{ fontWeight: "bold" }}
+              >
+                طرحواره رها شدگی
+              </Typography>
 
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  flexWrap: "wrap",
+                  fontSize: "0.8rem",
+                  margin: "8px 0",
+                  color: "#999",
+                }}
+              >
+                <AccessAlarm />
+                <span>۲ دقیقه</span>
+              </div>
+            </CardContent>
+            <CardActions>
+              <Button size='small'>شروع</Button>
+            </CardActions>
+          </Card>
+        </div>
+      </div>
+
+      <div
+        style={{ fontWeight: "bold", fontSize: "1.2rem", marginTop: "16px" }}
+      >
+        افسردگی
+      </div>
+      <div>
+        <div style={{ overflow: "auto", whiteSpace: "nowrap" }}>
+          <Card
+            style={{
+              display: "inline-block",
+              marginLeft: "8px",
+              width: "180px",
+            }}
+          >
+            <CardMedia
+              sx={{ height: 140 }}
+              image='https://nextluxury.com/wp-content/uploads/couple-not-talking-to-each-other-after-quarrel.jpg'
+            />
+            <CardContent sx={{ paddingBottom: "0px" }}>
+              <Typography
+                variant='body2'
+                sx={{ fontWeight: "bold" }}
+              >
+                طرحواره رها شدگی
+              </Typography>
+
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  flexWrap: "wrap",
+                  fontSize: "0.8rem",
+                  margin: "8px 0",
+                  color: "#999",
+                }}
+              >
+                <AccessAlarm />
+                <span>۲ دقیقه</span>
+              </div>
+            </CardContent>
+            <CardActions>
+              <Button size='small'>شروع</Button>
+            </CardActions>
+          </Card>
+          <Card
+            style={{
+              display: "inline-block",
+              marginLeft: "8px",
+              width: "180px",
+            }}
+          >
+            <CardMedia
+              sx={{ height: 140 }}
+              image='https://nextluxury.com/wp-content/uploads/couple-not-talking-to-each-other-after-quarrel.jpg'
+            />
+            <CardContent sx={{ paddingBottom: "0px" }}>
+              <Typography
+                variant='body2'
+                sx={{ fontWeight: "bold" }}
+              >
+                طرحواره رها شدگی
+              </Typography>
+
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  flexWrap: "wrap",
+                  fontSize: "0.8rem",
+                  margin: "8px 0",
+                  color: "#999",
+                }}
+              >
+                <AccessAlarm />
+                <span>۲ دقیقه</span>
+              </div>
+            </CardContent>
+            <CardActions>
+              <Button size='small'>شروع</Button>
+            </CardActions>
+          </Card>
           <Card
             style={{
               display: "inline-block",
--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/filter-app-front/",
+  // base: "/filter-app-front/",
 });
