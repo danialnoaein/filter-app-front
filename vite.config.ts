c22416f08f93909f6dd2608e830b9173f7d43018
Danial <sdnoaein@gmail.com>
2023-09-15 00:12:52 +0330

    fix: add dev mode base url config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "https://danialnoaein.github.io/filter-app-front/",
+
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    return {
+      // dev specific config
+      base: "/",
+    };
+  } else {
+    // command === 'build'
+    return {
+      // build specific config
+      base: "https://danialnoaein.github.io/filter-app-front/",
+    };
+  }
 });
